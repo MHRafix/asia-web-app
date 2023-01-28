00f159f557f24cfea06a3961a61c648a1dfcfe00
MHRafix <fmhrafiz27@gmail.com>
2023-01-28 16:08:16 +0600

    router instance error fix:-

--- src/pages/tour-packages/[id].tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from '@mantine/core';
-import { NextPage } from 'next';
-import Router from 'next/router';
-
-const SingleTour: NextPage = () => {
-	return <Container size='lg'>{Router.query.id}</Container>;
-};
-
-export default SingleTour;
