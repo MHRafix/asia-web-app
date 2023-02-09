30f7d15eeeb6b663b7267cc93671feb2b134c485
MHRafix <fmhrafiz27@gmail.com>
2023-02-09 16:50:58 +0600

    single package footer done:-

---
+++ src/components/custom/SinglePackages/StickyPriceArea.tsx
@@ -0,0 +1,30 @@
+import { Box, Button, Container, Flex, Footer, Text } from '@mantine/core';
+import React from 'react';
+
+const StickyPriceArea: React.FC<{}> = () => {
+	return (
+		<Footer height={70} withBorder={false} px={10} pt={10}>
+			<Container size='lg' px='xs'>
+				<div className='xs:w-full sm:w-10/12 lg:w-8/12 mx-auto grid gap-5'>
+					<Flex justify='space-between' align='center'>
+						<Box>
+							<Text fz={20} fw={700} ff='Poppins'>
+								$6656 <span className='text-sm'>/ person</span>
+							</Text>
+							<Text size='sm' fw={500}>
+								Mar 28 - Apr 06
+							</Text>
+						</Box>
+						<Box>
+							<Button color='teal' radius={100}>
+								Book Now
+							</Button>
+						</Box>
+					</Flex>
+				</div>
+			</Container>
+		</Footer>
+	);
+};
+
+export default StickyPriceArea;
--- src/pages/tour-packages/[id].tsx
+++ src/pages/tour-packages/[id].tsx
@@ -1,3 +1,4 @@
+import StickyPriceArea from '@/components/custom/SinglePackages/StickyPriceArea';
 import ClientAppLayout from '@/components/layout/ClientLayout/ClientAppLayout';
 import {
 	ActionIcon,
@@ -23,135 +24,143 @@ import { TbMapSearch, TbPlaneDeparture } from 'react-icons/tb';
 const SingleTourPage: NextPage = () => {
 	return (
 		<ClientAppLayout>
-			<Container size='lg' p='sm'>
-				<div className='flex items-center '>
-					<div className='xs:w-full sm:w-10/12 lg:w-8/12 mx-auto grid gap-5 xs:mt-2 md:mt-10'>
-						{/* <ProfileDetails />
+			<>
+				<Container size='lg' p='sm'>
+					<div className='flex items-center '>
+						<div className='xs:w-full sm:w-10/12 lg:w-8/12 mx-auto grid gap-5 xs:mt-2 md:mt-10'>
+							{/* <ProfileDetails />
                 <ProfileUpdateForm /> */}
-						<div>
-							<Image
-								src='https://images.unsplash.com/photo-1613507681723-252eb6b66967?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTAzMzB8MHwxfGFsbHx8fHx8fHx8fDE2Mjc1OTMzMTQ&ixlib=rb-1.2.1&q=80&w=1080&utm_source=travelities&utm_medium=referral&utm_campaign=api-credit'
-								alt='single_image'
-								width={100}
-								height={100}
-								className='w-full h-[300px] rounded-2xl'
-							/>
-							<Flex justify='space-between' align='center' my={10} px={10}>
-								<Box>
-									<Title order={3}>This is title</Title>
-									<Flex gap={5} align='center' mt={3}>
-										<>
-											<MdLocationPin size={16} color='#FF597B' />
-											<span className='text-xs text-slate-400'>Canada.</span>
-										</>
-										<Text color='cyan' size='xs'>
-											<Rating
-												size='xs'
+							<div>
+								<Image
+									src='https://images.unsplash.com/photo-1613507681723-252eb6b66967?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTAzMzB8MHwxfGFsbHx8fHx8fHx8fDE2Mjc1OTMzMTQ&ixlib=rb-1.2.1&q=80&w=1080&utm_source=travelities&utm_medium=referral&utm_campaign=api-credit'
+									alt='single_image'
+									width={100}
+									height={100}
+									className='w-full h-[300px] rounded-2xl'
+								/>
+								<Flex justify='space-between' align='center' my={10} px={10}>
+									<Box>
+										<Title order={3}>This is title</Title>
+										<Flex gap={5} align='center' mt={3}>
+											<>
+												<MdLocationPin size={16} color='#FF597B' />
+												<span className='text-xs text-slate-400'>Canada.</span>
+											</>
+											<Text color='cyan' size='xs'>
+												<Rating
+													size='xs'
+													color='teal'
+													value={4.5}
+													fractions={2}
+													readOnly
+												/>
+											</Text>
+											<Button
+												variant='light'
 												color='teal'
-												value={4.5}
-												fractions={2}
-												readOnly
-											/>
-										</Text>
-										<Button
-											variant='light'
-											color='teal'
-											fw={500}
-											rightIcon={<RiShareForward2Line size={20} />}
-										>
-											Share
-										</Button>
-									</Flex>
-								</Box>
+												fw={500}
+												rightIcon={<RiShareForward2Line size={20} />}
+											>
+												Share
+											</Button>
+										</Flex>
+									</Box>
 
-								<Box>
-									<Tooltip label='Map direction'>
-										<ActionIcon
-											color='teal'
-											size='xl'
-											radius='lg'
-											variant='filled'
-										>
-											<TbMapSearch size={25} />
-										</ActionIcon>
-									</Tooltip>
-								</Box>
-							</Flex>
-							<Divider my='sm' color='#e1e1e1' />
-							<div className='px-[10px]'>
-								<Title order={3} my={10}>
-									Tour Outline
-								</Title>
-								<Timeline color='teal' active={1} lineWidth={2} bulletSize={22}>
-									<Timeline.Item
-										bullet={<TbPlaneDeparture size={30} />}
-										bulletSize={55}
+									<Box>
+										<Tooltip label='Map direction'>
+											<ActionIcon
+												color='teal'
+												size='xl'
+												radius='lg'
+												variant='filled'
+											>
+												<TbMapSearch size={25} />
+											</ActionIcon>
+										</Tooltip>
+									</Box>
+								</Flex>
+								<Divider my='sm' color='#e1e1e1' />
+								<div className='px-[10px]'>
+									<Title order={3} my={10}>
+										Tour Outline
+									</Title>
+									<Timeline
+										color='teal'
+										active={1}
+										lineWidth={2}
+										bulletSize={22}
 									>
-										<Card px='lg' radius='md' mx={10}>
-											<Flex justify='space-between' align='center'>
-												<Text fw={500}>Travel from Dhaka</Text>
-												<Button variant='light' color='gray'>
-													<Title order={5} ff='Poppins' color='#444'>
-														Tue, Mar 28
-													</Title>
-												</Button>
-											</Flex>
+										<Timeline.Item
+											bullet={<TbPlaneDeparture size={30} />}
+											bulletSize={55}
+										>
+											<Card px='lg' radius='md' mx={10} shadow='lg'>
+												<Flex justify='space-between' align='center'>
+													<Text fw={500}>Travel from Dhaka</Text>
+													<Button variant='light' color='gray'>
+														<Title order={5} ff='Poppins' color='#444'>
+															Tue, Mar 28
+														</Title>
+													</Button>
+												</Flex>
 
-											<Divider my='xs' color='#e1e1e1' />
+												<Divider my='xs' color='#e1e1e1' />
 
-											<div>
-												<div className='grid md:grid-cols-3 gap-5'>
-													<Flex align='center' gap={8}>
-														<Image
-															src={planeImage}
-															alt='transportIcon'
-															width={40}
-															height={40}
-														/>
-														Qatar Airlines
-													</Flex>
-													<div>
-														<Text fz={15} fw={500}>
-															New York LGA - Turkey
-														</Text>
-														<Text size='sm' color='#c1c1c1'>
-															03:29 pm
-														</Text>
-													</div>
-													<div className='md:text-right'>
-														<Text fz={15} fw={500}>
-															2 stops
-														</Text>
-														<Text size='sm' color='#c1c1c1'>
-															12h 45m
-														</Text>
+												<div>
+													<div className='grid md:grid-cols-3 gap-5 '>
+														<Flex align='center' gap={8}>
+															<Image
+																src={planeImage}
+																alt='transportIcon'
+																width={40}
+																height={40}
+															/>
+															Qatar Airlines
+														</Flex>
+														<div>
+															<Text fz={15} fw={500}>
+																New York LGA - Turkey
+															</Text>
+															<Text size='sm' color='#c1c1c1'>
+																03:29 pm
+															</Text>
+														</div>
+														<div className='md:text-right'>
+															<Text fz={15} fw={500}>
+																2 stops
+															</Text>
+															<Text size='sm' color='#c1c1c1'>
+																12h 45m
+															</Text>
+														</div>
 													</div>
 												</div>
-											</div>
-										</Card>
-									</Timeline.Item>
+											</Card>
+										</Timeline.Item>
 
-									<Timeline.Item
-										title='Avatar'
-										bulletSize={55}
-										bullet={
-											<Avatar
-												size={55}
-												radius='xl'
-												src='https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4'
-											/>
-										}
-									>
-										<Text color='dimmed' size='sm'>
-											Timeline bullet as avatar image
-										</Text>
-									</Timeline.Item>
-								</Timeline>
+										<Timeline.Item
+											title='Avatar'
+											bulletSize={55}
+											bullet={
+												<Avatar
+													size={55}
+													radius='xl'
+													src='https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4'
+												/>
+											}
+										>
+											<Text color='dimmed' size='sm'>
+												Timeline bullet as avatar image
+											</Text>
+										</Timeline.Item>
+									</Timeline>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</Container>
+				</Container>
+				<StickyPriceArea />
+			</>
 		</ClientAppLayout>
 	);
 };
